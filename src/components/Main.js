0a7506b1148f3e15a33739d9caf9bff81c2d3e0c
Mohamed <Mohamed@mohameds-mbp.home>
2019-08-23 20:33:58 -0400

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useState } from 'react';
 import Search from './Search'
 import StockList from './StockList.js'
-import Fetch from './Fetch'
 
 
 function Main() {
